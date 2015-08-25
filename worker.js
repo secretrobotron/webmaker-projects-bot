e00e27e52e117f751fd839783f5ecf2847d29d58
secretrobotron <secretrobotron@gmail.com>
2015-08-25 12:00:53 -0400

    irc client stays awake

--- worker.js
+++ worker.js
@@ -5,6 +5,33 @@ var request = require('request');
 var pgConnectionString = process.env.PG_CONN_STRING;
 var sleepInterval = process.env.SLEEP_DURATION;                            // in milliseconds
 
+var _ircClient;
+
+function getIRCClient (callback) {
+  if (!_ircClient) {
+    console.log('Connecting to irc @ ' + process.env.IRC_SERVER + ' as ' + process.env.IRC_NICK + '...');
+    _ircClient = new irc.Client(process.env.IRC_SERVER, process.env.IRC_NICK, {
+      channels: [process.env.IRC_CHANNEL],
+      secure: true,
+      port: 6697
+    });
+
+    _ircClient.addListener('registered', function (message) {
+      callback(_ircClient);
+    });
+
+    _ircClient.addListener('error', function(message) {
+      console.error('IRC client error: ', message);
+      _ircClient.disconnect();
+      _ircClient = null;
+    });
+  }
+  else {
+    callback(_ircClient);
+  }
+}
+
+
 function getOneProject () {
   console.log('Waking up!');
 
@@ -42,45 +69,37 @@ function getOneProject () {
       var project = result.rows[0];
 
       if (project) {
-        console.log('Connecting to irc @ ' + process.env.IRC_SERVER + ' as ' + process.env.IRC_NICK + '...');
-        var ircClient = new irc.Client(process.env.IRC_SERVER, process.env.IRC_NICK, {
-          channels: [process.env.IRC_CHANNEL]
-        });
+        getIRCClient(function (ircClient) {
+          console.log('Found project ' + project.id + '! Updating db entry...');
+          client.query('UPDATE projects SET date_used = current_timestamp WHERE id = ' + project.id);
 
-        ircClient.addListener('error', function(message) {
-          console.error('IRC client error: ', message);
-        });
+          console.log('Getting project data from Webmaker...');
+          request(process.env.WEBMAKER_API_PREFIX + '/projects/' + project.project_id, function (error, response, body) {
 
-        console.log('Found project ' + project.id + '! Updating db entry...');
-        client.query('UPDATE projects SET date_used = current_timestamp WHERE id = ' + project.id);
+            if (error) {
+              console.error('Couldn\'t get project information from Webmaker API: ' + project.id);
+              console.error(error);
+              done();
+              return;
+            }
 
-        console.log('Getting project data from Webmaker...');
-        request(process.env.WEBMAKER_API_PREFIX + '/projects/' + project.project_id, function (error, response, body) {
+            var projectData = JSON.parse(body);
 
-          if (error) {
-            console.error('Couldn\'t get project information from Webmaker API: ' + project.id);
-            console.error(error);
+            if (projectData.status !== 'success') {
+              console.error('Webmaker API server responded unhappily.');
+              console.error(projectData);
+              done();
+              return;
+            }
+
+            console.log('Talking about it on IRC...');
+            var projectURL = process.env.WEBMAKER_PLAYER_PREFIX + '?user=' + projectData.project.user_id + '&project=' + project.project_id;
+            ircClient.say(process.env.IRC_CHANNEL, project.comment + ': ' + projectURL);
+
+            console.log('Done :). Sleeping...');
             done();
-            return;
-          }
-
-          var projectData = JSON.parse(body);
-
-          if (projectData.status !== 'success') {
-            console.error('Webmaker API server responded unhappily.');
-            console.error(projectData);
-            done();
-            return;
-          }
-
-          console.log('Talking about it on IRC...');
-          var projectURL = process.env.WEBMAKER_PLAYER_PREFIX + '?user=' + projectData.project.user_id + '&project=' + project.project_id;
-          ircClient.say(process.env.IRC_CHANNEL, project.comment + ': ' + projectURL);
-
-          console.log('Done :). Sleeping...');
-          ircClient.disconnect();
-          done();
-          setTimeout(getOneProject, sleepInterval);
+            setTimeout(getOneProject, sleepInterval);
+          });
         });
       }
       else {
